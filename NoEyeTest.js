e6bef6df4e712141dfd0bed1d5e8c5d4fcdd8133
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-02-20 14:40:47 -0500

    Update: Randomize progression, notification formatting
    + Create chance for `God Progs`
     - Randomize the % chance of god progs
    + Create notification for count of god progs
    + Re-designed notification system for fun, now bolding data keys and separating each on separate lines.
    + Adjusted +5 hard-limit to affect every prog-script eligible player
     - All players will have a 70% chance of a skill that is going up 5+ to change (between 1-5)
    + Adjusted the original penalty/limit of 26-29 being blocked from everything except 'endurance' and 'oldAgeKeys';
         - Players aged 26-29 are now on a decreasing linear function to allow a progression of the physical skills, starting at 70% chance @ 26 y/o
    + Removed restriction/limit of 30+ y/o players from progression a physical skill trait.
     + Change: Players aged 35+now  have a random, 1-5% chance to prog a physical skill trait [speed, strength, jump etc]
    + Collecting current season from within BBGM now, no manual edits required
    +In-File versioning, comment changes
    + Add missing exp. to if statement

--- NoEyeTest.js
+++ NoEyeTest.js
@@ -1,5 +1,6 @@
 /**
- * NoEyeTest: BBGM Prog Script
+ * @fileoverview
+ * NoEyeTest: BBGM Prog Script | v.2.0.0
  * This script is used to calculate the 'Prog Range' (PR) for a player.
  * A prog range is how low, or high high a player can progress in the off-season.
  * The prog range is calculated by taking the player's PER from the previous season
@@ -9,187 +10,212 @@
  */
 
 /**
- * @function customNotification
+ * Creates a notification into the game's log.
+ * @function sendProgNotification
  * @param {number} pid - Player ID
  * @param {string} notiMsg - Notification Message
- *
- * Credit to TheProgMaestro for this function. Minimal change here, saved me time reading on the notification system myself.
  */
-async function customNotification(pid, notiMsg) {
-    const p = await bbgm.idb.cache.players.get(pid);
-    const { tid } = p;
-    await bbgm.idb.cache.players.put(p);
-    await bbgm.logEvent({
-        type: 'Progs',
-        text: `<a href="${bbgm.helpers.leagueUrl([
-            'player',
-            p.pid,
-        ])}">${p.firstName} ${
-            p.lastName
-        }</a> Prog Details:<br/>${notiMsg}.`,
-        showNotification: true,
-        pids: [p.pid],
-        tids: [tid],
-        persistent: false,
-        score: 20,
-    });
+async function sendProgNotification(player, progRange, ageRange, per, godProg) {
+  if (!player) {
+    return;
+  }
+  const { pid, firstName, lastName, born, tid } = player;
+  if (!pid) {
+    return;
+  }
+  const notiTitle = godProg ? "God Progged!<br/>Prog Info:" : "Prog Info:";
+  const ageRangeFull = ageRange || `N/A`;
+  const perFull = per ? per.toFixed(2) : `N/A`;
+  await bbgm.idb.cache.players.put(player);
+  await bbgm.logEvent({
+    type: "Progs",
+    text: `<a href="${bbgm.helpers.leagueUrl([
+      "player",
+      pid,
+    ])}">\n${firstName} ${lastName} ${notiTitle}</a><br/><b>Range:</b> ${JSON.stringify(
+      progRange
+    )}<br/><b>
+        PER:
+        </b> ${perFull}<br/><b>
+        Age Group:
+        </b> ${ageRangeFull}`,
+    showNotification: true,
+    pids: [pid],
+    tids: [tid],
+    persistent: false,
+    score: 20,
+  });
 }
-/* global bbgm */ // Disable eslint error for referencing bbgm's cache system, it's defined in the BBGM codebase.
+let godProgCount = 0;
+let god;
 async function runProgs() {
-    const players = await bbgm.idb.cache.players.getAll(); // Collect all players in the game via the cache.
-    const seasonYr = 2013; //! MUST be changed before running progs to match the prior season year
-    const notification = customNotification;
+  const players = await bbgm.idb.cache.players.getAll(); // Collect all players in the game via the cache.
+  const SZN = bbgm.g.get("season");
+  const seasonYr = SZN - 1;
+  const notification = sendProgNotification;
+
+  for await (const p of players) {
     const ageFlags = {};
     ageFlags.thirty = false;
     ageFlags.twentySix = false;
-    // Iterate through the players
-    for await (const p of players) {
-        // Only control progs for players that do not have the 'classic' note on them;
-        // Fixed: Ignore rookie players by checking the draftYear.
-        if (p.note !== 'classic' && p.draft.year !== seasonYr) {
-            const name = `${p.firstName} ${p.lastName}`;
-            let per = 0;
-            // Filter (into an array) the stats for the player that meet our criteria: Last season, non-playoffs, and there has to be a PER value (this is to avoid grabbing the most recent season's stats, which, at the time of progs, will be completely empty)
-            const playerStats = p.stats.filter(
-                (stat) =>
-                    stat.season === seasonYr &&
-                    stat.per !== 0 &&
-                    !stat.playoffs
-            );
-            /**
-             * Every season creates 1 object to collect stats in for the player.
-             * If the player is traded mid-season, there will be duplicate object to collect stats for the player relating to the new team, but for the same season.
-             * This originally created an issue with the script, as PER was being grabbed incorrectly, or even worse, resulting in a NaN or 0 value, which would create a severely negative prog range.
-             * To fix this, we check the Filter (playerStats array) created earlier to see if there are more than 2 (stats) objects in the array. If so, we average the PER values from the 2 objects.
-             * Edits must be made to handle players traded more than once mid-season.
-             */
-            if (playerStats.length > 1) {
-                per = Math.fround(
-                    (playerStats[0].per + playerStats[1].per) / 2
-                );
-            }
 
-            // 1 Stat object, no need to average; just grab the PER value from the object
-            else if (playerStats.length === 1) {
-                per = Math.fround(playerStats[0].per);
-            }
-            // # Players with 0 PER should receive normal BBGM progs as a fallback.
-            if (per === 0) {
-                await notification(
-                    p.pid,
-                    'No PER found. Skipped altering their progs.'
-                );
-                continue;
-            }
-            // ? Determine player age
-            const age = bbgm.g.get('season') - p.born.year;
-            // ? Declare variable to be used later
-            let progRange = [0, 0];
-            if (p.ratings.length > 1) {
-                p.ratings.pop();
-                bbgm.player.addRatingsRow(p);
-                const ratings = p.ratings.at(-1);
-                // ? 26-29
-                if (age > 25 && age < 30) {
-                    ageFlags.twentySix = true;
-                    // ? 30+
-                } else if (age >= 30) {
-                    ageFlags.thirty = true;
-                }
+    if (p.note !== "classic" && p.draft.year !== seasonYr) {
+      const name = `${p.firstName} ${p.lastName}`;
+      let per = 0;
+      const playerStats = p.stats.filter(
+        (stat) => stat.season === seasonYr && stat.per !== 0 && !stat.playoffs
+      );
 
-                // # 26 to 30
-                if (age > 25 && age < 31) {
-                    progRange = [per / 5 - 7, per / 4 - 1];
-                    progRange = progRange.map((value) =>
-                        Math.round(value)
-                    );
-                    await notification(
-                        p.pid,
-                        `PER: ${per.toFixed(
-                            2
-                        )}<br/>Prog Range: ${progRange} [25-30]`
-                    );
-                }
+      if (playerStats.length > 0) {
+        per =
+          playerStats.reduce((sum, stat) => sum + stat.per, 0) /
+          playerStats.length;
+      }
 
-                // # 31 to 34
-                else if (age >= 31 && age < 35) {
-                    progRange = [per / 6 - 7, per / 4 - 2];
-                    progRange = progRange.map((value) =>
-                        Math.round(value)
-                    );
-                    await console.log(
-                        `${name}:\nPER: ${per}\nProg Range: ${progRange} [31-35]`
-                    );
-                    await notification(
-                        p.pid,
-                        `PER: ${per.toFixed(
-                            2
-                        )}<br/>Prog Range: ${progRange} [31-35]`
-                    );
-                }
+      if (playerStats.length > 1) {
+        per = Math.fround((playerStats[0].per + playerStats[1].per) / 2);
+      } else if (playerStats.length === 1) {
+        per = Math.fround(playerStats[0].per);
+      }
 
-                // # 35 and beyond
-                else if (age >= 35) {
-                    progRange = [per / 6 - 8, per / 7 - 1];
-                    progRange = progRange.map((value) =>
-                        Math.round(value)
-                    );
-                    await notification(
-                        p.pid,
-                        `PER: ${per.toFixed(
-                            2
-                        )}<br/>Prog Range: ${progRange} [35+]`
-                    );
-                }
-                const keys = [
-                    'diq',
-                    'dnk',
-                    'drb',
-                    'endu',
-                    'fg',
-                    'ft',
-                    'ins',
-                    'jmp',
-                    'oiq',
-                    'pss',
-                    'reb',
-                    'spd',
-                    'stre',
-                    'tp',
-                ];
-                let prog;
-                let oldAgeKeys = [`spd`, `stre`, `jmp`, `endu`];
-                let midAgeKeys = [`spd`, `stre`, `jmp`];
-                for (const key of keys) {
-                    // # If the player's `progRange` highest number is 5 or above, randomly select skils to not progress
-                    if (progRange[1] >= 5) {
-                        if (Math.random() < 0.5) {
-                            continue;
-                        }
-                    }
-                    // ? 30+ yr olds can't progress 'spd', 'stre', 'jmp', or 'endu' skills
-                    if (ageFlags.thirty && oldAgeKeys.includes(key)) {
-                        continue;
-                        // ? Restrict the same as 'oldAgeKeys' except `endu` (endurance) for 26-29 yr olds
-                    } else if (
-                        ageFlags.twentySix &&
-                        midAgeKeys.includes(key)
-                    ) {
-                        // # Use a number between 0 and 1
-                        prog = Math.random();
-                    } else {
-                        prog = bbgm.random.randInt(...progRange);
-                    }
-                    ratings[key] = bbgm.player.limitRating(
-                        ratings[key] + prog
-                    );
-                }
-                await bbgm.player.develop(p, 0);
-                await bbgm.player.updateValues(p);
-                await bbgm.idb.cache.players.put(p);
-            }
+      // # Players with 0 PER should receive normal BBGM progs.
+      if (per === 0) {
+        await notification(
+          p.pid,
+          "No PER found. Skipped altering their progs."
+        );
+        continue;
+      }
+      // ? Determine player age
+      const age = SZN - p.born.year;
+      // ? Declare variable to be used later
+      let progRange = [0, 0];
+      if (p.ratings.length > 1) {
+        p.ratings.pop();
+        bbgm.player.addRatingsRow(p);
+        const ratings = p.ratings.at(-1);
+        // ? 26-29
+        if (age > 25 && age < 30) {
+          ageFlags.twentySix = true;
+          // ? 30+
+        } else if (age >= 30) {
+          ageFlags.thirty = true;
         }
+
+        // # 26 to 30
+        if (age > 25 && age < 31) {
+          progRange = [per / 5 - 7, per / 4 - 1];
+          progRange = progRange.map((value) => Math.round(value));
+          await notification(p, progRange, "26-30", per);
+        }
+
+        // # 31 to 34
+        else if (age >= 31 && age < 35) {
+          progRange = [per / 6 - 7, per / 4 - 2];
+          progRange = progRange.map((value) => Math.round(value));
+          await notification(p, progRange, "31-35", per);
+        }
+
+        // # 35 and beyond
+        else if (age >= 35) {
+          progRange = [per / 6 - 8, per / 7 - 1];
+          progRange = progRange.map((value) => Math.round(value));
+          await notification(p, progRange, "35+", per);
+        }
+
+        // # God Progs for players under 29
+        if (age < 29) {
+          // Customize the chance of getting a "God Prog" | Chance should be random between 0.01 and 0.05
+          godProgChance = Math.random() * 0.06 + 0.01;
+          if (Math.random() < godProgChance) {
+            const minGodProg = 7;
+            const maxGodProg = 15;
+            const randProg =
+              Math.floor(Math.random() * maxGodProg) + minGodProg;
+            progRange = [randProg, randProg];
+            await notification(p, progRange, "< 29", per, true);
+            godProgCount += 1;
+          }
+        }
+        const keys = [
+          "diq",
+          "dnk",
+          "drb",
+          "endu",
+          "fg",
+          "ft",
+          "ins",
+          "jmp",
+          "oiq",
+          "pss",
+          "reb",
+          "spd",
+          "stre",
+          "tp",
+        ];
+        let prog;
+        let oldAgeKeys = [`spd`, `stre`, `jmp`, `endu`];
+        let midAgeKeys = [`spd`, `stre`, `jmp`];
+        for await (const key of keys) {
+          // # 30 y/os: Control chance of progging physical skills
+          if (ageFlags.thirty && oldAgeKeys.includes(key) && progRange[0] > 0) {
+            // # Provide anywhere between a 1 - 5% chance to prog a physical skill
+            const oldProgPhys = Math.random() * 0.06 + 0.01;
+            if (Math.random() < oldProgPhys) {
+              // # Limit the highest of either prog in the progRange property to 3
+              if (progRange[1] > 3) {
+                progRange[1] = 3;
+              } else if (progRange[0] > 3) {
+                progRange[0] = 3;
+              }
+            } else {
+              continue;
+            }
+          }
+          prog = bbgm.random.randInt(...progRange);
+
+          // ? A specific skill receiving a prog of 5 or more has a 70% chance of being changed
+          if (prog >= 5) {
+            if (Math.random() < 0.7) {
+              prog = randomNum(1, 5);
+            }
+          }
+
+          // ? Players age 26 start with a 70% chance to progress `spd`, `stre` and `jmp` skills. This chance decreases with age to
+          if (ageFlags.twentySix && midAgeKeys.includes(key) && prog > 0) {
+            // Decreasing linear function for age 26 to 29
+            const ageFactor = 0.7 - (age - 26) * 0.1;
+            const probProgression = Math.max(ageFactor, 0.2);
+            if (Math.random() > probProgression) {
+              continue;
+            }
+          }
+
+          ratings[key] = bbgm.player.limitRating(ratings[key] + prog);
+        }
+
+        await bbgm.player.develop(p, 0);
+        await bbgm.player.updateValues(p);
+        await bbgm.idb.cache.players.put(p);
+      }
     }
+  }
 }
 
+function randomNum(a, b) {
+  return Math.floor(Math.random() * b) + a;
+}
+
+const logGodProgs = async () => {
+  await bbgm.logEvent({
+    type: `God Progs`,
+    text: `God Prog Count This Run: ${godProgCount}`,
+    showNotification: true,
+    persistent: false,
+    score: 20,
+    pids: [0],
+  });
+};
+
 await runProgs(); // Run the script
+await logGodProgs(); // Log the number of God Progs
