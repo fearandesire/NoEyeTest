8d4aebcaac448766e979c3ae29cf60a1bc49d52c
fearandesire <63979371+fearandesire@users.noreply.github.com>
2023-04-04 21:08:06 -0400

    test: :sparkles: Add code snippet for worker console
    Adding in the code that needs to be run in the `Worker Console` for those who wish to test the script and lack access to a script that will manipulate the JSON for them.

---
+++ WorkerConsole.js
@@ -0,0 +1,12 @@
+// Providing the code snippet to 'flag' players for `NoEyeTest` to be tested without using an external script.
+// Run this in the 'Worker Console' in the BBGM Website.
+var players = await bbgm.idb.cache.players.getAll();
+const seasonObj = await bbgm.idb.cache.gameAttributes.get(`season`);
+const season = seasonObj.value;
+for (const p of players) {
+  const age = Number(season) - Number(p.born.year);
+  // Anyone above this age will be effected by `NoEyeTest`
+  if (age > 25) {
+    p.note = "classic";
+  }
+}
